7c1225c27d2a41722c32ef75bc8c44674a62c44f
YoskoBG <darkgt31252@outlook.com>
2026-03-07 20:41:51 +0300

    ConfigService.cs

---
+++ LeapAI.Net/Config/ConfigService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace OnAirLight.CommandLine.Config
+{
+    public static class ConfigService
+    {
+        public static AppConfig LoadAppSettings(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets<Program>()
+                .AddCommandLine(args)
+                .Build();
+
+
+            var config = new AppConfig();
+
+            builder.Bind(config);
+
+            return config;
+        }
+    }
+}
