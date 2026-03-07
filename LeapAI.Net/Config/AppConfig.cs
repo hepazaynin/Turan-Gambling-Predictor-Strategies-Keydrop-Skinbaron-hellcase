122cd82518fa7f3629c67c3baa80b66392d31b36
YoskoBG <darkgt31252@outlook.com>
2026-03-07 20:41:49 +0300

    AppConfig.cs

---
+++ LeapAI.Net/Config/AppConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace OnAirLight.CommandLine.Config
+{
+    public class AppConfig
+    {
+        public Azure Azure { get; set; }
+        public Hubitat Hubitat { get; set; }
+    }
+
+    public class Azure
+    {
+        public string AppId { get; set; }
+        public string TenantId { get; set; }
+        public string Scopes { get; set; }
+        public string UserId { get; set; }
+        public string[] AllScopes => Scopes.Split(';');
+    }
+
+    public class Hubitat
+    {
+        public string Id { get; set; }
+        public string Token { get; set; }
+        public string NameOrIp { get; set; }
+        public string DeviceName { get; set; }
+        public Dictionary<string, string> StatusMap { get; set; }
+    }
+}
